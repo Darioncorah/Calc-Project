1a41e49c8357ea02b1feffb49ff259a0ea23b81b
Darioncorah <daricora@hotmaail.com>
2020-09-29 12:39:27 -0600

    Finished Console App

--- cli/Program.cs
+++ cli/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Calculator;
 
 namespace cli
 {
@@ -10,6 +11,41 @@ namespace cli
     {
         static void Main(string[] args)
         {
+            Calc cal = new Calc();
+            Console.WriteLine("type ? for help");
+            switch (args[0].ToLower())
+            {
+                case "?":
+                    Console.WriteLine("add Num1 Num2 -- Adds numbers");
+                    Console.WriteLine("sub Num1 Num2 -- Subtracts numbers");
+                    Console.WriteLine("div Num1 Num2 -- Divides numbers");
+                    Console.WriteLine("mult Num1 Num2 -- Multiplys numbers");
+                    Console.WriteLine("dec2hex Num1 -- coverts a number to Hexadecimal");
+                    Console.WriteLine("hex2dec Hex -- converts Hexadecimal to number");
+                    break;
+                case "add":
+                    Console.WriteLine(cal.Add(Convert.ToDouble(args[1]), Convert.ToDouble(args[2])));
+                    break;
+                case "sub":
+                    Console.WriteLine(cal.Sub(Convert.ToDouble(args[1]), Convert.ToDouble(args[2])));
+                    break;
+                case "mult":
+                    Console.WriteLine(cal.Mult(Convert.ToDouble(args[1]), Convert.ToDouble(args[2])));
+                    break;
+                case "div":
+                    Console.WriteLine(cal.Div(Convert.ToDouble(args[1]), Convert.ToDouble(args[2])));
+                    break;
+                case "dec2hex":
+                    Console.WriteLine(cal.Dec2Hex(Convert.ToInt32(args[1])));
+                    break;
+                case "hex2dec":
+                    Console.WriteLine(cal.Dec2Hex(args[1]));
+                    break;
+                default:
+                    Console.WriteLine("Incorrect Entry");
+                    break;
+            }
+            Console.ReadKey();
         }
     }
 }
